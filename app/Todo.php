e79463b6de02337ae681617b0dcdb4d9fee3b3e8
Will Storey <will@willstorey.ca>
2019-11-24 15:04:46 -0500

    Remade list model as ToDo since list is reserved

--- app/Todo.php
+++ app/Todo.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class list extends Model
+class Todo extends Model
 {
     //
 }
--- app/item.php
+++ app/item.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class item extends Model
+class Item extends Model
 {
     //
 }
