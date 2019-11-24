e04bcd1c08fbdd7134d5927e8a1a67c4848ca760
Will Storey <will@willstorey.ca>
2019-11-24 15:40:04 -0500

    Added Db seeder for todos, users and items

--- database/factories/TodoFactory.php
+++ database/factories/TodoFactory.php
@@ -3,7 +3,9 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Todo::class, function (Faker $faker) {
+    $userIds = \App\User::all()->pluck('id');
     return [
-        //
+        'name' => $faker->words(3, true),
+        'user_id' => $faker->randomElement($userIds)
     ];
 });
