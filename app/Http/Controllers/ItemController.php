3eb09f4fe68323912b8ec5e16bcca81f8514aca4
Nathan Jubenville <NathanJubenville@gmail.com>
2019-11-26 06:32:11 -0500

    Commit before merging

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,9 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Item;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
-    //
+
+    public function index()
+    {
+        return view('lists.index');
+    }
+
+    public function show(Item $item)
+    {
+        return view('item.show',compact('item'));
+    }
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function edit()
+    {
+        //
+    }
+
+
+    public function update()
+    {
+        //
+    }
+
+    public function destroy()
+    {
+        //
+    }
 }
