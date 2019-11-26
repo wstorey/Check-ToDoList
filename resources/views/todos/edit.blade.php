cf5366794ed0d5cee99cdc492d02a68eef64bbf7
Will Storey <will@willstorey.ca>
2019-11-26 14:25:56 -0500

    Todo CRUD operational

---
+++ resources/views/todos/edit.blade.php
@@ -0,0 +1,15 @@
+@extends('master')
+@section('title') Edit @endsection
+
+@section('content')
+    <div class="form-group">
+        <form method="POST" action="
+        {{ action('TodoController@update', $todo->id) }}">
+        {{ method_field('PATCH') }}
+            @include('partials.createForm',
+            ['buttonName' => 'Update',
+             'name' => $todo->name,
+             'title' => 'Update Item'])
+        </form>
+    </div>
+@endsection
