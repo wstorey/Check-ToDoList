68dc1557894e03a31ee467b0f25f72432e21aec1
Will Storey <will@willstorey.ca>
2019-11-28 19:51:50 -0500

    Updated TodoController@show so users cannot view lists they do not own

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+//        View::composer('master', function ($view) {
+//            $view->with('currentUser', \App\User::all()->where());
+//        });
     }
 }
